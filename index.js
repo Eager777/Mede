3ba77b02efaf3a0541f976fea8dd1994eb09ea00
Eager777 <146904270+Eager777@users.noreply.github.com>
2023-10-13 12:51:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// class in JS
+//------------
+
+//const mobil = (
+  //  trensmisi: 'Manual',
+    //bahanBakar: 'Bensin',
+    //m//esin: 1500,
+    //n/yalakanMesin: function (){
+      //  console.log{'Mobil ${this,transmisi'}
+
+   // }
+//
+
+
+class MObil {
+    constructer
+
+this.transmisi
+const mobil = new 
+
+class Toyota extends Mobil{
+    hsdfd
+
+}
+}
